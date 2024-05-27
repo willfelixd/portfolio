55c719011e295ca5bf8b0b002db156104b83f0f3
willfelixd <willfelixd@gmail.com>
2024-05-27 02:34:15 -0300

    Window when clicking on portfolio and services

--- assets/js/steller.js
+++ assets/js/steller.js
@@ -17,3 +17,12 @@ $(document).ready(function(){
       	} 
     });
 });
+// create page
+document.getElementById('togglePortfolio').addEventListener('click', function() {
+    var portfolioSection = document.getElementById('portfolio');
+    if (portfolioSection.style.display === 'none') {
+        portfolioSection.style.display = 'block'
+    } else {
+        portfolioSection.style.display = 'none'
+    }
+});
